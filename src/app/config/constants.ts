4010ad9a382ac1e084bea23c4ed2a3982cb7aa6a
IdanKalishov <106066496+IdanKalishov@users.noreply.github.com>
2022-05-23 17:18:29 +0300

    format

---
+++ src/app/config/constants.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@angular/core';
+@Injectable()
+export class Constants {
+  public readonly TELEGRAM_ENDPOINT: string = 'https://t.me/';
+}
--- src/app/services/supporters.service.ts
+++ src/app/services/supporters.service.ts
@@ -3,23 +3,28 @@ import { HttpClient } from '@angular/common/http';
 import { supporter } from '../models/supporter';
 import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Constants } from '../config/constants';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SupportersService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private constants: Constants) {}
 
   fetchAll(): Observable<supporter[]> {
-    const suffix = "supporters";
-    return this.http.get<supporter[]>(environment.BACKEND_URL + suffix).pipe(tap(_ => console.log("Fetched all supporters" )));
+    const suffix = 'supporters';
+    return this.http
+      .get<supporter[]>(environment.BACKEND_URL + suffix)
+      .pipe(tap((_) => console.log('Fetched all supporters')));
   }
 
   connectUserToSupporter(): void {
-    this.fetchAll().subscribe(supporters => {
-      const selectedSupporter =  supporters[Math.floor(Math.random()*supporters.length)];
-      window.open("https://t.me/" + selectedSupporter?.telegram_user)
-    })
+    this.fetchAll().subscribe((supporters) => {
+      const selectedSupporter =
+        supporters[Math.floor(Math.random() * supporters.length)];
+      window.open(
+        this.constants.TELEGRAM_ENDPOINT + selectedSupporter?.telegram_user
+      );
+    });
   }
 }
