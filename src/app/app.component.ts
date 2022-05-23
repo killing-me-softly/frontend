9e09516aa96b670d1b1f4e482015349e1d3e1059
Shahar Alaluf <alaluf99@gmail.com>
2022-05-23 12:08:57 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,98 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  menuItems = [
+    {
+      name: 'יומן',
+      link: '/business/my-diary',
+      iconClass: 'fas fa-calendar-week',
+      introID: 'intro-menu-diary'
+    },
+    {
+      name: 'העסק שלי',
+      iconClass: 'fas fa-home',
+      id: 'my-business',
+      subMenus: [
+        {
+          name: 'מבט כללי',
+          link: '/business/home',
+          iconClass: 'fas fa-binoculars',
+        },
+        {
+          name: 'תורים להיום',
+          link: '/business/today-appointments',
+          iconClass: 'fas fa-handshake'
+        },
+        {
+          name: 'השירותים שלי',
+          link: '/business/my-services',
+          iconClass: 'fas fa-address-card',
+          introID: 'intro-menu-services'
+        },
+        {
+          name: 'האילוצים שלי',
+          link: '/business/my-constraints',
+          iconClass: 'fas fa-calendar-times',
+          introID: 'intro-menu-constraints'
+        },
+        {
+          name: 'האירועים שלי',
+          specialFeature: true,
+          link: '/business/my-events',
+          iconClass: 'fas fa-users-class'
+        },
+        {
+          name: 'הצעות ייעול',
+          link: '/business/optimization-tips',
+          iconClass: 'fab fa-cloudscale'
+        },
+      ]
+    },
+    {
+      name: 'אישור תורים',
+      link: '/business/approve-appointments',
+      iconClass: 'fas fa-thumbs-up'
+    },
+    {
+      name: 'לקוחות',
+      link: '/business/my-customers',
+      iconClass: 'fas fa-users'
+    },
+    {
+      name: 'דוחות',
+      link: '/business/my-reports',
+      iconClass: 'fas fa-file-chart-line'
+    },
+    {
+      name: 'מיתוג העסק',
+      link: '/business/business-branding',
+      iconClass: 'fas fa-megaphone',
+    },
+    {
+      name: 'הגדרות',
+      iconClass: 'fas fa-cogs',
+      id: 'settings',
+      subMenus: [
+        {
+          name: 'הגדרות עסק',
+          link: '/business/business-settings',
+          iconClass: 'fas fa-briefcase',
+          introID: 'intro-menu-settings'
+        },
+        {
+          name: 'תשלומים',
+          link: '/business/payments',
+          iconClass: 'fal fa-credit-card'
+        }
+      ]
+    },
+    {
+      name: 'מה חדש?',
+      link: '/business/whats-new',
+      iconClass: 'fas fa-lightbulb-on',
+    },
+  ];
+
   constructor() {}
 }
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,25 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./customer-home/customer-home.module').then(m => m.CustomerHomeModule)
+      },
+      {
+        path: 'my-appointments',
+        loadChildren: () => import('./my-appointments/my-appointments.module').then( m => m.MyAppointmentsPageModule)
+      },
+      {
+        path: 'my-tickets',
+        loadChildren: () => import('./my-tickets/my-tickets.module').then( m => m.MyTicketsPageModule)
+      }
+    ],
   }
 ];
 
