5c4a9f4598ba2d10ab1a53b12b719589ee46641d
Eyal Azran <azran4u@gmail.com>
2022-05-24 16:10:39 +0300

    feat: add route to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,32 +4,35 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./container/container.module').then(m => m.ContainerPageModule)
+    loadChildren: () =>
+      import('./container/container.module').then((m) => m.ContainerPageModule),
   },
   {
     path: 'container',
-    loadChildren: () => import('./container/container.module').then(m => m.ContainerPageModule)
+    loadChildren: () =>
+      import('./container/container.module').then((m) => m.ContainerPageModule),
   },
   {
     path: 'log-in',
-    loadChildren: () => import('./log-in/log-in.module').then(m => m.LogInComponentModule)
+    loadChildren: () =>
+      import('./log-in/log-in.module').then((m) => m.LogInComponentModule),
   },
   {
     path: 'sign-in',
-    loadChildren: () => import('./sign-in/sign-in.module').then(m => m.SignInComponentModule)
+    loadChildren: () =>
+      import('./sign-in/sign-in.module').then((m) => m.SignInComponentModule),
   },
   {
     path: '',
-    redirectTo: 'container',
-    pathMatch: 'full'
+    redirectTo: 'log-in',
+    pathMatch: 'full',
   },
-
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
