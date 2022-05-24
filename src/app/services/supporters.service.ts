01fc6c2b8dfdb6a04dea7375ebafb0fdb5abf88b
IdanKalishov <106066496+IdanKalishov@users.noreply.github.com>
2022-05-24 11:24:28 +0300

    mocked telegram connection

--- src/app/services/supporters.service.ts
+++ src/app/services/supporters.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { supporter } from '../models/supporter';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Constants } from '../config/constants';
+
+const telegramUsersMock: Observable<supporter[]> = of([
+  {
+    name: 'aaa',
+    phone_number: '12345',
+    telegram_user: 'helpBot',
+  },
+]);
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +19,7 @@ import { Constants } from '../config/constants';
 export class SupportersService {
   constructor(private http: HttpClient, private constants: Constants) {}
 
-  fetchAll(): Observable<supporter[]> {
+  fetchSupporters(): Observable<supporter[]> {
     const suffix = 'supporters';
     return this.http
       .get<supporter[]>(environment.BACKEND_URL + suffix)
@@ -19,7 +27,8 @@ export class SupportersService {
   }
 
   connectUserToSupporter(): void {
-    this.fetchAll().subscribe((supporters) => {
+    //todo when the server is up switch to this.fetchSupporters
+    telegramUsersMock.subscribe((supporters) => {
       const selectedSupporter =
         supporters[Math.floor(Math.random() * supporters.length)];
       window.open(
